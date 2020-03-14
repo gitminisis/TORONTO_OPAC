4acd0f3520f90df085c99df4a2ba12e36a598845
opacteam <mike@minisisinc.com>
2020-03-14 08:38:45 -0700

    added filter

--- M2A/src/index.js
+++ M2A/src/index.js
@@ -5,8 +5,11 @@ import ReactDom from "react-dom";
 
 import Home from "./pages/home";
 import Summary from "./pages/summary";
+import Detail from "./pages/detail";
 if (document.getElementById("home")) {
   ReactDom.render(<Home />, document.getElementById("home"));
 } else if (document.getElementById("summary")) {
   ReactDom.render(<Summary />, document.getElementById("summary"));
+} else if (document.getElementById("detail")) {
+  ReactDom.render(<Detail />, document.getElementById("detail"));
 }
